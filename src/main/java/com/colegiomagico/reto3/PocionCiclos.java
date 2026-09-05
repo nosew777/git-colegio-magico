12ecdf41b3f73db087e966cb7f793de62de93f35
nosew777 <coca33691@gmail.com>
2026-09-05 16:23:36 +0000

    feat: solucion reto 3

--- src/main/java/com/colegiomagico/reto3/PocionCiclos.java
+++ src/main/java/com/colegiomagico/reto3/PocionCiclos.java
@@ -6,10 +6,17 @@ package com.colegiomagico.reto3;
 public class PocionCiclos {
 
     public static String preparar(int cantidad) {
-        return ""; // TODO: Cambia esto
-    }
+        String pociones ="";
+        for (int i = 0; i < cantidad; i++) {
+            pociones= pociones + "pocion ";
+        }
+        return pociones; // TODO: Cambia esto
 
+
+    }
     public static void main(String[] args) {
         System.out.println("Preparando 3 pociones: " + preparar(3));
+        
     }
+ 
 }
